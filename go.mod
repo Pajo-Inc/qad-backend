ef9c547fa1f25be60640bef14740f8a5b80f0c06
Patrik Potocki <rambo3swe@gmail.com>
2020-08-31 22:09:35 +0200

    managed to connect to rethinkdb

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/gin-gonic/gin v1.6.3 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/olahol/melody v0.0.0-20180227134253-7bd65910e5ab // indirect
+	gopkg.in/rethinkdb/rethinkdb-go.v6 v6.2.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,11 +1,35 @@
 package main
 
 import (
+	"fmt"
+	"log"
+
 	"github.com/gin-gonic/gin"
 	"github.com/olahol/melody"
+	r "gopkg.in/rethinkdb/rethinkdb-go.v6"
 )
 
 func main() {
+	session, err := r.Connect(r.ConnectOpts{
+		Address: "db", // endpoint without http
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	res, err := r.Expr("Hello World").Run(session)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var response string
+	err = res.One(&response)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(response)
+
 	r := gin.Default()
 	m := melody.New()
 	r.GET("/ws", func(c *gin.Context) {
