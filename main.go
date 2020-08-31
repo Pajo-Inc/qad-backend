32877ab3d5050ec52938d82d7dbf75909d852d7a
Patrik Potocki <rambo3swe@gmail.com>
2020-08-31 22:04:11 +0200

    setup basic env

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/olahol/melody"
+)
+
+func main() {
+	r := gin.Default()
+	m := melody.New()
+	r.GET("/ws", func(c *gin.Context) {
+		m.HandleRequest(c.Writer, c.Request)
+	})
+	m.HandleMessage(func(s *melody.Session, msg []byte) {
+		m.Broadcast(msg)
+	})
+	r.Run(":5000")
+}
